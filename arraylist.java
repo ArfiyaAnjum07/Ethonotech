39189aa2e1fe505621e50963d1855f586a85db75
ArfiyaAnjum07 <shaikarfiyaanjum@gmail.com>
2026-06-19 10:25:00 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+    public class arraylist
+    {
+        public static void main(String[] args)
+        {  
+            ArrayList<String>stud=new ArrayList<String>();
+            stud.add("Arfiya");
+            stud.add("Afshiya");
+            stud.add("saniya");
+            stud.add(0,"Arfiya");
+            System.out.println(stud);
+        }
+    } 
